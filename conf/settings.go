6dfe3dbd38e6f198d935245b9624d068b2341028
Myroslava Hrechyn <hrechyn.myroslava@gmail.com>
2020-05-28 03:05:59 +0300

    few changes

--- conf/settings.go
+++ conf/settings.go
@@ -1,7 +1,7 @@
 package conf
 
 const (
-	BOT_TOKEN = "1248237303:AAHKt_4kkr5qtr4JQnEYsDxT8atcOlRk9Mg"
-	BOT_PORT  = "8090"
-	WEB_HOOK  = "https://b47b73a4.ngrok.io"
+	BOT_TOKEN = ""
+	BOT_PORT  = ""
+	WEB_HOOK  = ""
 )
